5759f8886ffa0e2ee1416b22329d036f3ba85cea
a.kulikov <a.kulikov@vicont.ru>
2022-09-07 12:17:30 +0300

    Добавьте файлы проекта.

---
+++ HierarchicalTestProject/GeneralClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpf.Grid;
+
+namespace HierarchicalTest
+{
+    public static class RND
+    {
+        private static Random rnd = new Random();
+        private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        public static string GetNewString(int length)
+        {
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+        public static int GetNewInt(int i1, int i2)
+        {
+            return rnd.Next(i1, i2);
+        }
+    }
+
+    //public class CustomChildrenSelector : IChildNodesSelector
+    //{
+    //    public IEnumerable SelectChildren(object item)
+    //    {
+    //        if (item is BaseClass) { return null; }
+    //        else if (item is SubClass) { return (item as SubClass).Collection1; }
+    //        else if (item is GeneralClass) { return (item as GeneralClass).Collection0; }
+    //        return null;
+    //    }
+    //}
+
+    public class BaseClass
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public BaseClass()
+        {
+            Name = RND.GetNewString(RND.GetNewInt(5,10));
+            ID = RND.GetNewInt(-1000, 1000);
+        }
+    }
+
+    public class SubClass : BaseClass
+    {
+        public ObservableCollection<BaseClass> Collection1 { get; set; } = new ObservableCollection<BaseClass>();
+        public SubClass()
+        {
+            int cycles = RND.GetNewInt(5, 10);
+            for (int i = 0; i < cycles; i++)
+            {
+                BaseClass insert_obj = new BaseClass();
+                Collection1.Add(insert_obj);
+            }
+        }
+    }
+
+    public class GeneralClass : BaseClass
+    {
+        public ObservableCollection<SubClass> Collection0 { get; set; } = new ObservableCollection<SubClass>();
+        public GeneralClass()
+        {
+            int cycles = RND.GetNewInt(5, 10);
+            for (int i = 0; i < cycles; i++)
+            {
+                SubClass insert_obj = new SubClass();
+                Collection0.Add(insert_obj);
+            }
+        }
+    }
+}
---
+++ HierarchicalTestProject/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.Export;
+using DevExpress.Mvvm;
+using DevExpress.XtraPrinting;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HierarchicalTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<GeneralClass> G { get; set; } = new ObservableCollection<GeneralClass>();
+        
+        public MainWindow()
+        {
+            G.Add(new GeneralClass());
+            G.Add(new GeneralClass());
+            G.Add(new GeneralClass());
+            this.DataContext = this;
+            InitializeComponent();
+            TRVIEW.ExpandAllNodes();    // If comment this line, the TreeViewControl nodes cannot be expanded
+        }
+
+
+    }
+}
